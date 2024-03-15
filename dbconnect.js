95d726e9b1b1be878153cb25943b4487a64f2061
DeadStreap <74311984+DeadStreap@users.noreply.github.com>
2024-03-15 23:25:49 +0300

    optimize dbconnection code

--- dbconnect.js
+++ dbconnect.js
@@ -1,18 +1,22 @@
-const mysql = require('mysql')
+const mysql = require('mysql2');
 
-const con = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'bykbbpls6huwf48phbzz-mysql.services.clever-cloud.com',
     user: 'ulsipa4bzelzl1wv',
     password: "IrzQYvw3ZKlGebdHvcti",
-    database: "bykbbpls6huwf48phbzz"
-})
+    database: "bykbbpls6huwf48phbzz",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
 
-con.connect((err) =>{
-    if(err){
-        console.log('ERROR' + err)
-    }else{
-        console.log("DB was successfully connected")
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to database: ', err);
+        return;
     }
-})
+    console.log('Database connected!');
+    connection.release();
+});
 
-module.exports = con
+module.exports = pool.promise();
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-var express = require('express')
-const Router = require('./routes/router')
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const cors = require('cors');
+const Router = require('./routes/router');
+const PORT = process.env.PORT || 8080;
 
-var app = express()
+const app = express();
 
-var cors = require('cors')
-app.use(cors())
-
-app.use(express.json())
-app.use('/api', Router)
+app.use(cors());
+app.use(express.json());
+app.use('/api', Router);
 
 app.listen(PORT, (err) => {
     if(err){
